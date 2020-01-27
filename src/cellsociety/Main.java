2215b09de94b59f211997f9eb5bca25e65d2dfed
Libba Lawrence <ll313@duke.edu>
2020-01-27 10:56:20 -0500

    added in Main methods

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -4,6 +4,18 @@ package cellsociety;
  * Feel free to completely change this code or delete it entirely. 
  */
 public class Main {
+
+
+    public void setUpInitialScreen(){}
+
+    public void ReadFile() {}
+
+    public void step (double elapsedTime) {}
+
+
+
+
+
     /**
      * Start of the program.
      */
