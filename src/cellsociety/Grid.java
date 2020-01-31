4a205c787760cd119808e0eb8047b57f415af13a
Claudia Chapman <claudia.chapman@duke.edu>
2020-01-30 20:39:03 -0500

    wrote updateGrid method

--- src/cellsociety/Grid.java
+++ src/cellsociety/Grid.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.lang.reflect.Method;
 
 public class Grid {
-    ArrayList<Cell> myArrayOfCells = new ArrayList<Cell>();
+    ArrayList<ArrayList<Cell>> myArrayOfCells = new ArrayList<ArrayList<Cell>>();
 
     public Grid(int height, int width, int vCellNum, int hCellNum, String gameVariation){
 
 
+    }
+    
+    public void updateGrid(){
+        for(ArrayList<Cell> innerList : myArrayOfCells){
+            for(Cell cell: innerList){
+                cell.findNeighbors();
+                cell.updateCellValue();
+            }
+        }
     }
 }
