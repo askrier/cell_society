61c549ea2c222c09b329e2152e0b3ee359f2338c
Claudia Chapman <claudia.chapman@duke.edu>
2020-02-01 17:52:18 -0500

    finished game of life I think

--- src/cellsociety/GameOfLifeCell.java
+++ src/cellsociety/GameOfLifeCell.java
@@ -38,7 +38,15 @@ public class GameOfLifeCell extends Cell{
 
 
     @Override
-    protected void findNeighbors(ArrayList<ArrayList<Cell>> gridOfCells, int cellColumn, int cellRow) {
-
+    public void findNeighbors(ArrayList<ArrayList<Cell>> gridOfCells, int cellColumn, int cellRow){
+        int[] northNeighbor = {cellColumn, cellRow-1};
+        int[] southNeighbor = {cellColumn, cellRow+1};
+        int[] eastNeighbor = {cellColumn+1, cellRow};
+        int[] westNeighbor = {cellColumn-1, cellRow-1};
+        populateNeighbors(gridOfCells, northNeighbor);
+        populateNeighbors(gridOfCells, southNeighbor);
+        populateNeighbors(gridOfCells, eastNeighbor);
+        populateNeighbors(gridOfCells, westNeighbor);
     }
+    
 }
