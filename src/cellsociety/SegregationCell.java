fd52f17108cdf600cdddaf6341b95d7f3429cfb7
Alexander Uzochukwu <aou@duke.edu>
2020-02-01 09:58:55 -0500

    Edited error in FireCell

--- src/cellsociety/SegregationCell.java
+++ src/cellsociety/SegregationCell.java
@@ -1,18 +1,34 @@
 package cellsociety;
 
+import java.util.ArrayList;
+
 public class SegregationCell extends Cell {
 
-    public SegregationCell(){
-
+    public SegregationCell(int cellState){
+        super(cellState);
+        neighborArray = new ArrayList<Cell>();
     }
 
+    /**
+     * @see Cell#updateCellValue()
+     */
     @Override
     public void updateCellValue(){
 
     }
 
+    /**
+     * @see Cell#findNeighbors(ArrayList, int, int)
+     */
     @Override
-    public void findNeighbors(){
-
+    public void findNeighbors(ArrayList<ArrayList<Cell>> gridOfCells, int cellColumn, int cellRow){
+        int[] northNeighbor = {cellColumn, cellRow-1};
+        int[] southNeighbor = {cellColumn, cellRow+1};
+        int[] eastNeighbor = {cellColumn+1, cellRow};
+        int[] westNeighbor = {cellColumn-1, cellRow-1};
+        populateNeighbors(gridOfCells, northNeighbor);
+        populateNeighbors(gridOfCells, southNeighbor);
+        populateNeighbors(gridOfCells, eastNeighbor);
+        populateNeighbors(gridOfCells, westNeighbor);
     }
 }
