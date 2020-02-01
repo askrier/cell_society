d1a07bb9edb4a7e8099e69999fe68026d828c180
Claudia Chapman <claudia.chapman@duke.edu>
2020-02-01 13:11:29 -0500

    created and edited Grid class, wrote updateGrid and working on constructor

--- src/cellsociety/Grid.java
+++ src/cellsociety/Grid.java
@@ -1,17 +1,22 @@
 package cellsociety;
 
 import java.util.ArrayList;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 
 public class Grid {
     ArrayList<ArrayList<Cell>> myArrayOfCells = new ArrayList<ArrayList<Cell>>();
 
-    public Grid(int height, int width, int vCellNum, int hCellNum, String gameVariation){
+    public Grid(int height, int width, int vCellNum, int hCellNum, String gameVariation) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        String clsName = "cellsociety.PercolationCell";
+        Class<?> myCellClass = Class.forName(gameVariation);
+        for(int i=0;i<vCellNum;i++){
+            myArrayOfCells.add(new ArrayList<?>());
+        }
 
-
+        Object cell = (Object) myCellClass.getDeclaredConstructor().newInstance();
     }
-    
-    public void updateGrid(){
+
+    public void updateGrid()  {
         for(ArrayList<Cell> innerList : myArrayOfCells){
             for(Cell cell: innerList){
                 cell.findNeighbors();
