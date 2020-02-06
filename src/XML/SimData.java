cb030027cd5ac4fd3d77a93972b90ec73f3bfbce
Andrew Krier <ak513@duke.edu>
2020-02-06 17:33:22 -0500

    cleaned up simdata by removing unused methods and fixing constructor

--- src/XML/SimData.java
+++ src/XML/SimData.java
@@ -31,18 +31,17 @@ public class SimData {
     private String myVals;
     private double mySpreadProb;
     private List<List<Integer>> myCellVals;
-    private Map<String, String> myDataValues;
+//    private Map<String, String> myDataValues;
 
-    public SimData (String author, String simType, int cellSides, int cellRows, int cellColumns, String vals, double spreadProb, List<List<Integer>> cellVals) {
+    public SimData (String author, String simType, int cellSides, int cellRows, int cellColumns, String vals, double spreadProb) {
         mySimType = simType;
         myAuthor = author;
         myCellSides = cellSides;
         myCellRows = cellRows;
         myCellColumns = cellColumns;
-        //myCellVals = cellVals;
         myVals = vals;
         mySpreadProb = spreadProb;
-        myDataValues = new HashMap<>();
+//        myDataValues = new HashMap<>();
     }
 
     public SimData (Map<String, String> dataValues) {
@@ -52,19 +51,18 @@ public class SimData {
                 Integer.parseInt(dataValues.get(DATA_FIELDS.get(CELL_ROWS_INDEX))),
                 Integer.parseInt(dataValues.get(DATA_FIELDS.get(CELL_COLUMNS_INDEX))),
                 dataValues.get(DATA_FIELDS.get(VALS_INDEX)),
-                Float.parseFloat(dataValues.get(DATA_FIELDS.get(SPREAD_PROBABILITY_INDEX))),
-                null);
-        //System.out.println(dataValues.get(DATA_FIELDS.get(6)));
+                Float.parseFloat(dataValues.get(DATA_FIELDS.get(SPREAD_PROBABILITY_INDEX))));
         setVals();
-        myDataValues = dataValues;
+//        myDataValues = dataValues;
     }
 
     public String getSimType () { return mySimType; }
 
     public String getAuthor () { return myAuthor; }
-
+/*
     public int getShape () { return myCellSides; }
 
+ */
     public int getRows () { return myCellRows; }
 
     public int getColumns () { return myCellColumns; }
@@ -72,7 +70,7 @@ public class SimData {
     public double getSpreadProb () { return mySpreadProb; }
 
     public List<List<Integer>> getValList () { return myCellVals; }
-
+/*
     public int getCellVal (int x_val, int y_val) {
 
         if(outOfBounds(x_val, y_val)) return -1;
@@ -87,6 +85,8 @@ public class SimData {
 
     }
 
+ */
+
     private void setVals () {
         int temp;
         myCellVals = new ArrayList<>();
@@ -97,12 +97,8 @@ public class SimData {
                 temp = Integer.parseInt(myVals.substring(0, 1));
                 row.add(temp);
                 myVals = myVals.substring(1);
-              //  System.out.println(temp);
-
             }
             myCellVals.add(row);
-            //row.clear();
-           // System.out.println(myCellVals);
         }
     }
 
